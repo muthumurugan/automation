221374bb36c9f853d5c4401dd924bec211333e06
muthumurugan <muthumurugan.ramalingam@hirepro.in>
2023-10-17 13:14:33 +0530

    changed encryption delete query

--- SCRIPTS/DB_DELETE/encryption_delete.py
+++ SCRIPTS/DB_DELETE/encryption_delete.py
@@ -12,9 +12,10 @@ class EncryptionDelete:
     def encryption_delete():
         db_connection = ams_db_connection()
         cursor = db_connection.cursor()
-        query = 'delete from candidates where hp_dec(usn) = ("ENCRYPTION_CHECK2") and tenant_id=248;'
 
-        print(query)
+        query = 'delete from candidates where ' \
+                'email1 = "enc_str:::aiSSXIZOrLo6zWtF/Dzi+RFKdp2BAV6o8abacVu7GYCB4Nbjqw2085XMQXBYCWk9" ' \
+                'and tenant_id=1787;'
         cursor.execute(query)
         db_connection.commit()
         db_connection.close()
