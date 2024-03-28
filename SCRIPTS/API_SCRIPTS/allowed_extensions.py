930f1862432a5d73f1b4c540f69cccf648b72bec
muthumurugan <muthumurugan.ramalingam@hirepro.in>
2024-03-28 12:12:53 +0530

    code refactored and added comments to allowd extensions file

--- SCRIPTS/API_SCRIPTS/allowed_extensions.py
+++ SCRIPTS/API_SCRIPTS/allowed_extensions.py
@@ -6,8 +6,8 @@ from SCRIPTS.COMMON.io_path import *
 
 
 class AllowedFileExtensions:
-
     def __init__(self):
+        # List of allowed file extensions
         self.allowed_extensions = [".doc", ".rtf", ".dot", ".docx", ".docm", ".dotx", ".dotm", ".docb", ".pdf", ".xls",
                                    ".xlt", ".xlm", ".xlsx", ".xlsm", ".xltx", ".xltm", ".xlsb", ".xla", ".xlam", ".xll",
                                    ".xlw", ".ppt", ".pot", ".pps", ".pptx", ".pptm", ".potx", ".potm", ".ppam", ".ppsx",
@@ -17,33 +17,41 @@ class AllowedFileExtensions:
         requests.packages.urllib3.disable_warnings()
         self.row_size = 2
         write_excel_object.save_result(output_path_allowed_extension)
-        header = ["Allowed Extensions"]
-        write_excel_object.write_headers_for_scripts(0, 0, header, write_excel_object.black_color_bold)
-        header1 = ["Extension Type", "Status", "File Name", "Expected Status", "Actual Status", "API Response"]
-        write_excel_object.write_headers_for_scripts(1, 0, header1, write_excel_object.black_color_bold)
+        self.write_headers()
+
+    def write_headers(self):
+        # Writing headers in the Excel file
+        headers = ["Allowed Extensions"]
+        write_excel_object.write_headers_for_scripts(0, 0, headers, write_excel_object.black_color_bold)
+        headers1 = ["Extension Type", "Status", "File Name", "Expected Status", "Actual Status", "API Response"]
+        write_excel_object.write_headers_for_scripts(1, 0, headers1, write_excel_object.black_color_bold)
 
     def validate_files(self, token, excel_input):
-        write_excel_object.current_status = "Pass"
-        write_excel_object.current_status_color = write_excel_object.green_color
-        print(excel_input.get('filePathName'))
+        # Validating files and writing results
         file_path = input_path_allowed_extension_files % (excel_input.get('filePathName'))
         file_name = excel_input.get('fileName')
-        print(file_name)
         resp = crpo_common_obj.upload_files(token, file_name, file_path)
+        actual_status = self.get_actual_status(resp)
+        self.write_results(excel_input, actual_status)
+
+    def get_actual_status(self, resp):
+        # Determining actual status based on API response
+        error_desc = resp['error']['errorDescription'] if 'error' in resp else ''
         if resp.get('status') == 'OK' and resp['data']:
-            actual_status = 'allowed'
-        elif "DisAllowed fileExtension" in (resp['error']['errorDescription']):
-            actual_status = 'disallowed - fileExtension for the control'
-        elif 'File extension does not match file format' in resp['error']['errorDescription']:
-            actual_status = "disallowed - File extension does not match file format"
-        elif 'UnAcceptable file format' in resp['error']['errorDescription']:
-            actual_status = "UnAcceptable file format"
-        elif 'DisAllowedFileExtFiles' in resp['error']['errorDescription']:
-            actual_status = resp['error']['errorDescription']
-        elif 'ExtNFileFormatMismatchedFiles' in resp['error']['errorDescription']:
-            actual_status = resp['error']['errorDescription']
+            return 'allowed'
+        elif "DisAllowed fileExtension" in error_desc:
+            return 'disallowed - fileExtension for the control'
+        elif 'File extension does not match file format' in error_desc:
+            return "disallowed - File extension does not match file format"
+        elif 'UnAcceptable file format' in error_desc:
+            return "UnAcceptable file format"
+        elif 'DisAllowedFileExtFiles' in error_desc or 'ExtNFileFormatMismatchedFiles' in error_desc:
+            return error_desc
         else:
-            actual_status = "status unknown"
+            return "status unknown"
+
+    def write_results(self, excel_input, actual_status):
+        # Writing results in the Excel file
         write_excel_object.compare_results_and_write_vertically(excel_input.get('extensionType'), None, self.row_size, 0)
         write_excel_object.compare_results_and_write_vertically(excel_input.get('fileName'), None, self.row_size, 2)
         write_excel_object.compare_results_and_write_vertically(excel_input.get('expectedStatus'), actual_status,
@@ -51,13 +59,20 @@ class AllowedFileExtensions:
         write_excel_object.compare_results_and_write_vertically(write_excel_object.current_status, None, self.row_size, 1)
         self.row_size += 1
 
-
+# Initializing AllowedFileExtensions object
 allowed_ext_obj = AllowedFileExtensions()
+
+# Logging in to CRPO
 login_token = crpo_common_obj.login_to_crpo(cred_crpo_admin.get('user'), cred_crpo_admin.get('password'),
                                             cred_crpo_admin.get('tenant'))
 
+# Reading data from Excel file
 excel_read_obj.excel_read(input_path_allowed_extension, 0)
 excel_data = excel_read_obj.details
+
+# Validating files and writing results
 for data in excel_data:
     allowed_ext_obj.validate_files(login_token, data)
+
+# Writing overall status in the Excel file
 write_excel_object.write_overall_status(testcases_count=40)
